9ee7978a8de1fa78e6c92df01a64f8eebb334ca1
Brian Grant <brian@skeletonjack.net>
2019-11-09 10:37:15 -0600

    Refactored to use functions & remove some duplication

--- dnd.py
+++ dnd.py
@@ -3,25 +3,40 @@ import os
 import sys
 import time
 
-os.system('defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true')
-os.system('defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date "`date -u +\"%Y-%m-%d %H:%M:%S +0000\"`"')
-os.system('killall NotificationCenter')
+defaults_starter = 'defaults -currentHost write ' \
+                   '~/Library/Preferences/ByHost/com.apple.notificationcenterui'
 
+dnd_cmd = f'{defaults_starter} doNotDisturb -boolean'
+dnd_date_cmd = f'{defaults_starter} doNotDisturbDate -date ' \
+               f'"`date -u +\"%Y-%m-%d %H:%M:%S +0000\"`"'
+kill_cmd = 'killall NotificationCenter'
 pomodoro_length = 25
 
-minutes = 0
 
+def enable_dnd():
+    os.system(dnd_cmd + ' true')
+    os.system(dnd_date_cmd)
+    os.system(kill_cmd)
+
+
+def disable_dnd():
+    os.system(dnd_cmd + ' false')
+    os.system(kill_cmd)
+
+
+def pause_for_focus_time(minutes):
+    seconds_to_sleep = minutes * 60
+    print(f'DND is enabled; sleeping for {minutes} minutes...')
+    time.sleep(seconds_to_sleep)
+    print('Waking up & switching off DND')
+
+
+input_minutes = 0
 if len(sys.argv) == 1:
-    minutes = pomodoro_length
+    input_minutes = pomodoro_length
 else:
-    minutes = int(sys.argv[1])
+    input_minutes = int(sys.argv[1])
 
-minutes_to_sleep = minutes * 60
-
-print(f'DND is enabled; sleeping for {minutes} minutes...')
-time.sleep(minutes_to_sleep)
-
-print('Waking up & switching off DND')
-
-os.system('defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean false')
-os.system('killall NotificationCenter')
+enable_dnd()
+pause_for_focus_time(input_minutes)
+disable_dnd()
