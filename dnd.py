7bd9d1310671a94a95bc70594322eadcfb5f05fd
Brian Grant <brian@skeletonjack.net>
2019-11-12 14:47:35 -0600

    Hardcoded some application groups

--- dnd.py
+++ dnd.py
@@ -10,7 +10,14 @@ dnd_cmd = f'{defaults_starter} doNotDisturb -boolean'
 dnd_date_cmd = f'{defaults_starter} doNotDisturbDate -date ' \
                f'"`date -u +\"%Y-%m-%d %H:%M:%S +0000\"`"'
 kill_cmd = 'killall NotificationCenter'
-application_names = ['Microsoft Teams', 'Microsoft Outlook']
+
+application_groups = {
+    'default': ['Microsoft Teams', 'Microsoft Outlook'],
+    'none': [],
+    'email': ['Microsoft Outlook'],
+    'chat': ['Microsoft Teams']
+}
+
 pomodoro_length = 25
 
 
@@ -20,23 +27,20 @@ def tell_applications(app_list, what_to_tell):
                       f"\t{what_to_tell}\n" \
                       f"end tell\n'"
         # print(script)
-
-        # TODO allow for applications from being quit/relaunched
-        #  to be managed by parameters
-        # os.system(script)
+        os.system(script)
 
 
-def enable_dnd():   # maybe start_focus instead?
+def enable_dnd(apps):   # maybe start_focus instead?
     os.system(dnd_cmd + ' true')
     os.system(dnd_date_cmd)
     os.system(kill_cmd)
-    tell_applications(application_names, 'quit')
+    tell_applications(apps, 'quit')
 
 
-def disable_dnd():
+def disable_dnd(apps):
     os.system(dnd_cmd + ' false')
     os.system(kill_cmd)
-    tell_applications(application_names, 'activate')
+    tell_applications(apps, 'activate')
 
 
 def pause_for_focus_time(minutes):
@@ -46,12 +50,21 @@ def pause_for_focus_time(minutes):
     print('Waking up & switching off DND')
 
 
-input_minutes = 0
-if len(sys.argv) == 1:
-    input_minutes = pomodoro_length
-else:
-    input_minutes = int(sys.argv[1])
+input_minutes = pomodoro_length
+apps = application_groups.get('default')
 
-enable_dnd()
+if len(sys.argv) > 1:
+    args = sys.argv[1:len(sys.argv)]
+    for arg in args:
+        print(f'arg is {arg}')
+        if arg.isnumeric():
+            print(f'{arg} is numeric')
+            input_minutes = int(arg)
+        elif application_groups.get(arg) != None:
+            print(f'identified an application group:  {arg}')
+            apps = application_groups.get(arg)
+
+
+enable_dnd(apps)
 pause_for_focus_time(input_minutes)
-disable_dnd()
+disable_dnd(apps)
